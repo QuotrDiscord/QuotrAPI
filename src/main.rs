320554ebfeef7b67359d81207b09bc127a0594f8
H-274 <GitHub@jsmarier.com>
2023-12-15 23:11:06 -0500

    Added database migration execution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use actix_web::web::ServiceConfig;
+use migration::{Migrator, MigratorTrait};
 use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_secrets::SecretStore;
 
 #[shuttle_runtime::main]
-async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+async fn main(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let db_url = secret_store
+        .get("DB_URL")
+        .expect("Could not find required secret DB_URL");
+
+    execute_migrations(&db_url)
+        .await
+        .expect("Failed to execute migrations");
+
     Ok(api::main().into())
+}
+
+async fn execute_migrations(database: &str) -> Result<(), migration::DbErr> {
+    let connection = sea_orm::Database::connect(&*database).await?;
+    Ok(Migrator::up(&connection, None).await?)
 }
